b7025f46641a876e065e14086e858b02894970ee
csap <csirintronsopon8@gmail.com>
2025-04-06 17:32:10 +0700

    fix: test failed

--- src/app.test.js
+++ src/app.test.js
@@ -1,4 +1,4 @@
-const { dayOfTheWeek } = require("./app");
+const { dayOfTheWeek, monthOfTheYear } = require("./app");
 
 test("getDay returns the long-format day of the week", () => {
   const day = dayOfTheWeek(new Date("3/11/2020"));
@@ -6,6 +6,6 @@ test("getDay returns the long-format day of the week", () => {
 });
 
 test("getDaty returns the month of the year", () => {
-  const month = dayOfTheWeek(new Date("3/11/2020"));
+  const month = monthOfTheYear(new Date("3/11/2020"));
   expect(month).toBe("November");
 });
